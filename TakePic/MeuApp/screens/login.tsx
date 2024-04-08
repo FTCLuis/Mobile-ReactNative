66139402b3e06b042f78e59d96590435fcbd3334
LuisaoxD <105399037+LuisaoxD@users.noreply.github.com>
2024-04-08 13:30:07 -0300

    Alterações Model Login

--- TakePic/MeuApp/screens/login.tsx
+++ TakePic/MeuApp/screens/login.tsx
@@ -4,18 +4,23 @@ import Header from '../components/Header/header';
 import Footer from '../components/Footer/footer'; 
 import LoginForm from '../components/LoginForm/loginform'; 
 
+const footerData = {
+  text: "Não possui conta? ",
+  text2: "cadastrar"
+}
+
 const LoginPage = () => {
-  return (
-    <View style={{flex: 1}}>
-      <Header />
-      <View style={{ paddingHorizontal: 20, flex: 1, marginTop: 26}}>
-        <Text style={{ fontSize: 38, marginBottom: 5, marginTop: 10, fontWeight: "bold" }}>Login</Text>
-        <View style={{width: 100, backgroundColor: 'hotpink', height: 2, marginBottom: 20 }} />
-        <LoginForm />
-        <Footer />
-      </View>
-    </View>
-  );
+    return (
+        <View style={{flex: 1}}>
+            <Header />
+            <View style={{ paddingHorizontal: 20, flex: 1, marginTop: 26}}>
+                <Text style={{ fontSize: 38, marginBottom: 5, marginTop: 10, fontWeight: "bold" }}>Login</Text>
+                <View style={{width: 100, backgroundColor: 'hotpink', height: 2, marginBottom: 20 }} />
+                <LoginForm />
+                <Footer data={footerData}/>
+            </View>
+        </View>
+    );
 };
 
 export default LoginPage;
